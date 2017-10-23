6b653bab0da57aff66940b8f40f98e22295e68e3
akm <akm2000@gmail.com>
2017-10-23 19:59:11 +0900

    Add FilterLink to filter todos

--- src/index.js
+++ src/index.js
@@ -5,9 +5,43 @@ import ReactDOM from 'react-dom';
 
 import {todo, todos, visibilityFilter, todoApp, store} from './todos';
 
+const FilterLink = ({
+  filter,
+  children
+}) => {
+  return (
+    <a href='#'
+      onClick={e => {
+        e.preventDefault();
+        store.dispatch({
+          type: 'SET_VISIBILITY_FILTER',
+          filter
+        });
+      }}
+      >
+      {children}
+    </a>
+  );
+};
+
+const getVisibleTodos = (
+  todos,
+  filter
+) => {
+  switch (filter) {
+  case 'SHOW_ALL':
+    return todos;
+  case 'SHOW_COMPLETED':
+    return todos.filter(t => t.completed);
+  case 'SHOW_ACTIVE':
+    return todos.filter(t => !t.completed);
+  }
+}
+
 let nextTodoId = 0;
 class TodoApp extends Component {
   render() {
+    const visibleTodos = getVisibleTodos(this.props.todos, this.props.visibilityFilter);
     return (
       <div>
         <input ref={node => {
@@ -24,7 +58,7 @@ class TodoApp extends Component {
           Add Todo
         </button>
         <ul>
-          {this.props.todos.map(todo => (
+          {visibleTodos.map(todo => (
             <li key={todo.id}
                 onClick={() => {
                   store.dispatch({
@@ -42,6 +76,27 @@ class TodoApp extends Component {
             </li>
           ))}
         </ul>
+        <p>
+          Show:
+          {' '}
+          <FilterLink
+            filter='SHOW_ALL'
+          >
+            All
+          </FilterLink>
+          {' '}
+          <FilterLink
+            filter='SHOW_ACTIVE'
+          >
+            Active
+          </FilterLink>
+          {' '}
+          <FilterLink
+            filter='SHOW_COMPLETED'
+          >
+            Completed
+          </FilterLink>
+        </p>
       </div>
     );
   }
@@ -50,7 +105,7 @@ class TodoApp extends Component {
 const render = () => {
   ReactDOM.render(
     <TodoApp
-      todos={store.getState().todos}
+      {...store.getState()}
     />,
     document.getElementById('root')
   );
