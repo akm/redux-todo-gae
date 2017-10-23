354fa3962e6145ba95f900d26989e1205bad33e9
akm <akm2000@gmail.com>
2017-10-23 17:58:02 +0900

    Implement toggleTodo with Object.assign

--- src/tests.js
+++ src/tests.js
@@ -59,4 +59,33 @@ const testIncrementCounter = () => {
 testAddCounter();
 testRemoveCounter();
 testIncrementCounter();
+
+// Toggle TODO
+
+const toggleTodo = (todo) => {
+  return Object.assign({}, todo, {
+    completed: !todo.completed
+  });
+};
+
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    test: 'Learn Redux',
+    completed: false,
+  };
+  const todoAfter = {
+    id: 0,
+    test: 'Learn Redux',
+    completed: true,
+  };
+
+  deepFreeze(todoBefore);
+
+  expect(
+    toggleTodo(todoBefore)
+  ).toEqual(todoAfter);
+}
+
+testToggleTodo()
 console.log("Test passed.")
