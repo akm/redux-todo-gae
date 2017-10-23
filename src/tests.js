817dac1beabdb1e556fa7c6fe0c32e92126bf285
akm <akm2000@gmail.com>
2017-10-23 17:52:39 +0900

    Add incrementCounter

--- src/tests.js
+++ src/tests.js
@@ -14,6 +14,14 @@ const removeCounter = (list, index) => {
   ];
 };
 
+const incrementCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    list[index] + 1,
+    ...list.slice(index + 1)
+  ];
+};
+
 const testAddCounter = () => {
   const listBefore = [];
   const listAfter = [0];
@@ -36,6 +44,19 @@ const testRemoveCounter = () => {
   ).toEqual(listAfter);
 }
 
+const testIncrementCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 11, 20];
+
+  deepFreeze(listBefore);
+
+  expect(
+    incrementCounter(listBefore, 1)
+  ).toEqual(listAfter);
+}
+
+
 testAddCounter();
 testRemoveCounter();
+testIncrementCounter();
 console.log("Test passed.")
