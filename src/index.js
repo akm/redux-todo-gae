d1fb6053c96aa160ddb6c8853d7b82f34c6239ae
akm <akm2000@gmail.com>
2017-10-23 19:37:32 +0900

    Implment TodoApp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
 // import expect from 'expect'
 import Redux, { createStore } from 'redux';
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import {todo, todos, visibilityFilter, todoApp, store} from './todos';
+
+let nextTodoId = 0;
+class TodoApp extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={() => {
+          store.dispatch({
+            type: 'ADD_TODO',
+            text: 'Test',
+            id: nextTodoId++
+          });
+        }}>
+          Add Todo
+        </button>
+        <ul>
+          {this.props.todos.map(todo => (
+            <li key={todo.id}>
+              {todo.text}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const render = () => {
+  ReactDOM.render(
+    <TodoApp
+      todos={store.getState().todos}
+    />,
+    document.getElementById('root')
+  );
+};
+
+store.subscribe(render);
+render();
