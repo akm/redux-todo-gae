b7c9a6d8f2f04a462e4b37129149bc61a507a7c2
akm <akm2000@gmail.com>
2017-10-23 17:34:27 +0900

    Add addCounter and its test

--- src/tests.js
+++ src/tests.js
@@ -1,4 +1,19 @@
 import expect from 'expect'
 
 console.log("Test started.")
+
+const addCounter = (list) => {
+  list.push(0);
+  return list
+};
+
+const testAddCounter = () => {
+  const listBefore = [];
+  const listAfter = [0];
+  expect(
+    addCounter(listBefore)
+  ).toEqual(listAfter);
+};
+
+testAddCounter()
 console.log("Test passed.")
