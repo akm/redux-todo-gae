ea609e4b0bf0efa3ff62889e3d401cd4e7cfee08
akm <akm2000@gmail.com>
2017-10-23 15:55:38 +0900

    Implement Store from scratch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import expect from 'expect'
-import Redux, { createStore } from 'redux'
+import Redux from 'redux'
 
 const counter = (state = 0, action) => {
   switch (action.type) {
@@ -11,6 +11,30 @@ const counter = (state = 0, action) => {
     return state;
   }
 }
+
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  }
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter(l => l != listener);
+    }
+  };
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+};
+
 
 const store = createStore(counter);
 
