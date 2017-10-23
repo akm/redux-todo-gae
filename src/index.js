edf1377557cec7818cea5f425bcd3ad50785669d
akm <akm2000@gmail.com>
2017-10-23 17:05:06 +0900

    Render by using react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import expect from 'expect'
-import Redux, { createStore } from 'redux'
+// import expect from 'expect'
+import Redux, { createStore } from 'redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const counter = (state = 0, action) => {
   switch (action.type) {
@@ -12,15 +14,38 @@ const counter = (state = 0, action) => {
   }
 }
 
+const Counter = ({
+  value,
+  onIncrement,
+  onDecrement
+}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+);
+
 const store = createStore(counter);
 
 const render = () => {
-  document.body.innerText = store.getState();
-}
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() =>
+        store.dispatch({
+          type: 'INCREMENT'
+        })
+      }
+      onDecrement={() =>
+        store.dispatch({
+          type: 'DECREMENT'
+        })
+      }
+    />,
+    document.getElementById('root')
+  );
+};
 
 store.subscribe(render);
 render();
-
-document.addEventListener('click', () => {
-  store.dispatch({ type: 'INCREMENT' });
-});
