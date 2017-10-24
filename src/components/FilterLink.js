6eeaa70c3df60e949c06a29b1ada0e6ba1768382
akm <akm2000@gmail.com>
2017-10-24 17:43:27 +0900

    Rewrite FilterLink.js by using Link from react-router-dom

--- src/components/FilterLink.js
+++ src/components/FilterLink.js
@@ -1,40 +1,16 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import Link from './Link'
-import { setVisibilityFilter } from '../actions/TodoActions'
-
-class FilterLink extends Component {
-  componentDidMount() {
-    const { store } = this.context;
-    this.unsubscribe = store.subscribe(() =>
-      this.forceUpdate()
-    );
-  }
-
-  componentWilUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    const props = this.props;
-    const { store } = this.context;
-    const state = store.getState();
-
-    return (
-      <Link
-        active={props.filter ===state.visibilityFilter}
-        onClick={() =>
-          store.dispatch(setVisibilityFilter(props.filter))
-        }
-      >
-        {props.children}
-      </Link>
-    );
-  }
-}
-FilterLink.contextTypes = {
-  store: PropTypes.object
-}
+const FilterLink = ({ filter, children }) => (
+  <Link
+    to={filter === 'all' ? '' : filter}
+    activeStyle={{
+      textDecoration: 'none',
+      color: 'black',
+    }}
+  >
+    {children}
+  </Link>
+)
 
 export default FilterLink
