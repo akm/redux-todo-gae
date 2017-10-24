db60bb90503d16c5e6a59e150eb727d2900aabf9
akm <akm2000@gmail.com>
2017-10-24 18:16:39 +0900

    Use alias name for BrowserRouter
    https://github.com/ReactTraining/react-router/blob/master/packages/react-router-dom/docs/guides/quick-start.md

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import App from './App'
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <BrowserRouter>
+    <Router>
       <Route path="/" component={App}/>
-    </BrowserRouter>
+    </Router>
   </Provider>
 );
 
