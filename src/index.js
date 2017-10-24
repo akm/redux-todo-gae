fa3901d502f3a2bae5f73b606668d1e054a502ee
akm <akm2000@gmail.com>
2017-10-24 11:37:09 +0900

    Remove render function and subscription
    Because VisibleTodoList and Footer subscribe by themselves

--- src/index.js
+++ src/index.js
@@ -195,14 +195,7 @@ const TodoApp = () => (
   </div>
 );
 
-const render = () => {
-  ReactDOM.render(
-    <TodoApp
-      {...store.getState()}
-    />,
-    document.getElementById('root')
-  );
-};
-
-store.subscribe(render);
-render();
+ReactDOM.render(
+  <TodoApp />,
+  document.getElementById('root')
+);
