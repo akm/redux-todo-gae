d3a994eddacb78005960e83bb51d8e2f74162ebd
akm <akm2000@gmail.com>
2017-10-24 16:42:10 +0900

    Extract src/configureStore.js from src/index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 // import expect from 'expect'
-import Redux, { createStore } from 'redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import throttle from 'lodash/throttle';
-
-import todoApp from './reducers/index';
-import { loadState, saveState } from './localStorage'
 
 import App from './components/App'
+import configureStore from './configureStore'
 
-const persistedState = loadState();
-const store = createStore(todoApp, persistedState)
-console.log(store.getState());
-
-store.subscribe(throttle(() => {
-  saveState({
-    todos: store.getState().todos
-  });
-}, 1000));
+const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
