0d6360e7d7ab8ee723b5c08b6138e86bac15669e
akm <akm2000@gmail.com>
2017-10-26 23:40:21 +0900

    Move getVisibleTodos from components/VisibleTodoList.js to reducers/todo.js

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -5,20 +5,6 @@ import { withRouter } from 'react-router';
 import TodoList from './TodoList'
 import { toggleTodo } from '../actions/TodoActions'
 
-const getVisibleTodos = (
-  todos,
-  filter
-) => {
-  switch (filter) {
-  case 'all':
-    return todos;
-  case 'completed':
-    return todos.filter(t => t.completed);
-  case 'active':
-    return todos.filter(t => !t.completed);
-  }
-}
-
 const mapSateToTodoListProps = (state, { match }) => ({
   todos: getVisibleTodos(state.todos, match.params.filter || 'all')
 });
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -35,3 +35,17 @@ const todos = (state = [], action) => {
 };
 
 export default todos
+
+export const getVisibleTodos = (
+  todos,
+  filter
+) => {
+  switch (filter) {
+  case 'all':
+    return todos;
+  case 'completed':
+    return todos.filter(t => t.completed);
+  case 'active':
+    return todos.filter(t => !t.completed);
+  }
+}
