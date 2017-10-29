6fbc85876be0c042abe1da5c6baab9159e017979
akm <akm2000@gmail.com>
2017-10-29 22:54:35 +0900

    Call fetchTodos and output to log on life cycle hooks

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -8,6 +8,20 @@ import { getVisibleTodos } from '../reducers';
 import { fetchTodos } from '../api';
 
 class VisibleTodoList extends Component {
+  componentDidMount() {
+    fetchTodos(this.props.filter).then(todos =>
+      console.log(this.props.filter, todos)
+    );
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.filter !== prevProps.filter) {
+      fetchTodos(this.props.filter).then(todos =>
+        console.log(this.props.filter, todos)
+      );
+    }
+  }
+
   render() {
     return <TodoList {...this.props} />;
   }
