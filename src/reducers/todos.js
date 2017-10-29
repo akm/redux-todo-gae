f3b3e380c33ad315faaa163d65c263808d81d990
akm <akm2000@gmail.com>
2017-10-29 21:57:46 +0900

    Extract todo from todos.js into indepent todo.js

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,26 +1,5 @@
 import { combineReducers } from 'redux';
-
-export const todo = (state, action) => {
-  switch (action.type) {
-  case 'ADD_TODO':
-    return {
-      id: action.id,
-      text: action.text,
-      completed: false
-    };
-  case 'TOGGLE_TODO':
-    if (state.id !== action.id) {
-      return state;
-    }
-
-    return {
-      ...state,
-      completed: !state.completed
-    }
-  default:
-    return state;
-  }
-};
+import todo from './todo';
 
 const byId = (state = {}, action) => {
   switch (action.type) {
