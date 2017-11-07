b2bbfd384f393b035cf76348db6b8704f51bdd5e
akm <akm2000@gmail.com>
2017-11-07 18:46:19 +0900

    Use redux-promise and redux-logger instead

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,40 +1,18 @@
 import Redux, { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 import todoApp from './reducers';
-
-const logger = (store) => (next) => {
-  if (!console.group) {
-    return next;
-  }
-
-  return (action) => {
-    console.group(action.type);
-    console.log('%c prev state', 'color: gray', store.getState());
-    console.log('%c action', 'color: blue', action);
-    const returnValue = next(action);
-    console.log('%c next state', 'color: green', store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
-  };
-};
-
-const promise = (store) => (next) => (action) => {
-  if (typeof action.then == 'function') {
-    return action.then(next);
-  }
-  return next;
-};
 
 const configureStore = () => {
   const middlewares = [promise];
   if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(logger);
+    middlewares.push(createLogger());
   }
 
   const store = createStore(
     todoApp,
     applyMiddleware(...middlewares)
   );
-  wrapDispatchWithMiddlewares(store, middlewares);
 
   return store;
 }
