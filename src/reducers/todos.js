72236e04e1968f598ae26b3e9e2fae97a76ffd31
akm <akm2000@gmail.com>
2017-11-07 19:12:56 +0900

    Remove the process for ADD_TODO and TOGGLE_TODO and
    Add a process for RECEIVE_TODOS

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -3,12 +3,12 @@ import todo from './todo';
 
 const byId = (state = {}, action) => {
   switch (action.type) {
-  case 'ADD_TODO':
-  case 'TOGGLE_TODO':
-    return {
-      ...state,
-      [action.id]: todo(state[action.id], action),
-    };
+  case 'RECEIVE_TODOS':
+    const nextState = { ...state };
+    action.response.forEach(todo => {
+      nextState[todo.id] = todo;
+    });
+    return nextState;
   default:
     return state;
   }
