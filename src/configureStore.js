f31fd8ff842af5e7a621f682b45887a2ef79d9d3
akm <akm2000@gmail.com>
2017-11-07 18:30:44 +0900

    Fix indentation

--- src/configureStore.js
+++ src/configureStore.js
@@ -2,26 +2,26 @@ import Redux, { createStore } from 'redux';
 import todoApp from './reducers';
 
 const logger = (store) => (next) => {
-    if (!console.group) {
-      return next;
-    }
+  if (!console.group) {
+    return next;
+  }
 
-    return (action) => {
-      console.group(action.type);
-      console.log('%c prev state', 'color: gray', store.getState());
-      console.log('%c action', 'color: blue', action);
-      const returnValue = next(action);
-      console.log('%c next state', 'color: green', store.getState());
-      console.groupEnd(action.type);
-      return returnValue;
-    };
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = next(action);
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
 };
 
 const promise = (store) => (next) => (action) => {
-      if (typeof action.then == 'function') {
-        return action.then(next);
-      }
-      return next;
+  if (typeof action.then == 'function') {
+    return action.then(next);
+  }
+  return next;
 };
 
 const wrapDispatchWithMiddlewares = (store, middlewares) => {
