7b4c1356ae4e85b8a5eeced81340ffdb9bec2fc2
akm <akm2000@gmail.com>
2017-11-10 17:49:21 +0900

    :shower: Comment out dummy error

--- src/api/index.js
+++ src/api/index.js
@@ -24,9 +24,10 @@ const delay = (ms) =>
 
 export const fetchTodos = (filter) =>
   delay(500).then(() => {
-    if (Math.random() > 0.5) {
-      throw new Error('Boom!');
-    }
+    // // Uncomment the folowwings if you want throw error some times.
+    // if (Math.random() > 0.5) {
+    //   throw new Error('Boom!');
+    // }
 
     switch (filter) {
     case 'all':
