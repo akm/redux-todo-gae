ad54b74efdabd7d4eef4b3c42d118c64b23d6359
akm <akm2000@gmail.com>
2017-11-10 21:00:48 +0900

    :bug: Move FlatButton to inside of NavLink

--- src/components/FilterLink.js
+++ src/components/FilterLink.js
@@ -4,7 +4,6 @@ import { NavLink } from 'react-router-dom';
 import FlatButton from 'material-ui/FlatButton';
 
 const FilterLink = ({ filter, children }) => (
-  <FlatButton>
   <NavLink
     exact
     to={filter === 'all' ? '/' : `/${filter}`}
@@ -13,9 +12,8 @@ const FilterLink = ({ filter, children }) => (
       color: 'black',
     }}
   >
-    {children}
+    <FlatButton>{children}</FlatButton>
   </NavLink>
-  </FlatButton>
 )
 
 export default FilterLink
