7310960d1c4ea5565e60b0652b2693116573510e
akm <akm2000@gmail.com>
2017-11-10 10:18:44 +0900

    :+1: Don't require Todo#completed on parse

--- src/models/todo.go
+++ src/models/todo.go
@@ -13,7 +13,7 @@ import (
 type Todo struct {
 	ID        string    `json:"id" datastore:"-"`
 	Text      string    `json:"text" validate:"required"`
-	Completed bool      `json:"completed" validate:"required"`
+	Completed bool      `json:"completed"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
